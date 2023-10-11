236185a4a4e6d7961041da8db74b8a3757ce35d6
lovefirst02 <lovefirst02@gmail.com>
2023-10-11 18:31:48 +0800

    update to tw

--- src/module/snkrsData.js
+++ src/module/snkrsData.js
@@ -1,7 +1,7 @@
 const request = require('request')
 const rp = require('request-promise')
 
-const api = "https://api.nike.com/product_feed/threads/v2/?anchor=0&count=50&filter=marketplace%28MY%29&filter=language%28en-GB%29&filter=upcoming%28true%29&filter=channelId%28010794e5-35fe-4e32-aaff-cd2c74f89d61%29"
+const api = "https://api.nike.com/product_feed/threads/v3/?count=50&filter=marketplace%28TW%29&filter=language%28zh-Hant%29&filter=channelId%28010794e5-35fe-4e32-aaff-cd2c74f89d61%29"
 
 function getSnkrs(){
     return new Promise((resolve,reject) => {
